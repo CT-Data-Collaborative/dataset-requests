5861ee9158f187ba460b3d79c9f3dde9e421376a
Sasha Cuerda <sasha@mapconomy.com>
2015-08-25 21:27:54 +0200

    tweaking apphook

--- djangocms_dataset_requests/cms_apps.py
+++ djangocms_dataset_requests/cms_apps.py
@@ -1,8 +1,9 @@
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
 
 class DatasetRequestAndIssues(CMSApp):
-    name = 'Dataset Request and Issue Tracking'
+    name = _('Dataset Request and Issue Tracking')
     urls = ['dataset-requests.urls']
 
 apphook_pool.register(DatasetRequestAndIssues)
